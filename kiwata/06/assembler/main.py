ede23006f6f70a28611a9f7bebdd440495c5f35e
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-01-21 03:19:25 +0900

    update assembler

--- kiwata/06/assembler/main.py
+++ kiwata/06/assembler/main.py
@@ -1,6 +1,8 @@
 import sys
 
-from memonic_parser import get_advance
+from memonic_parser import (
+    SymbolTable,
+)
 
 def main():
     print(sys.argv)
@@ -14,7 +16,7 @@ def main():
     # Text processing
     del_commet = lambda value: value[:2] != '//'
     del_new_line = lambda value: value[:2] != '\n'
-    replace_new_line = lambda value: value.replace('\n', '')
+    replace_new_line = lambda value: value.replace('\n', '').strip().split(' ')[0]
     input_texts = list(
         map(
             replace_new_line, filter(
@@ -22,11 +24,29 @@ def main():
             )
         )
     )
-    print(input_texts)
+    print('-' * 10)
+    # Create Symbol Table.
+    address = 0
+    symbol_table = SymbolTable()
     for input_text in input_texts:
-        command = get_advance(input_text)
-        print(input_text, command)
-        commands.append(command)
+        command_type = symbol_table.get_command_type(input_text)
+        if command_type == symbol_table.A_COMMAND:
+            address += 1
+        elif command_type == symbol_table.L_COMMAND:
+            symbol_table.add_symbol_table(
+                input_text[1:-1],
+                format(address, '016b')
+            )
+        else:
+            address += 1
+    print(symbol_table.get_symbol_table())
+    print('-' * 10)
+    # Create binary.
+    for input_text in input_texts:
+        command_type = symbol_table.get_command_type(input_text)
+        if command_type != symbol_table.L_COMMAND:
+            command = symbol_table.get_advance(input_text)
+            commands.append(command)
     create_hack_file(output_file_name, commands)
 
 
--- kiwata/06/assembler/memonic_parser.py
+++ kiwata/06/assembler/memonic_parser.py
@@ -1,75 +1,105 @@
 from code import get_dest_binary, get_comp_binary, get_jump_binary
 
-A_COMMAND = 0
-C_COMMAND = 1
-L_COMMAND = 2
+
+class SymbolTable:
+    def __init__(self):
+        self.A_COMMAND = 0
+        self.C_COMMAND = 1
+        self.L_COMMAND = 2
+        self.DEFINED_SYMBOL_TABLE = {
+            'R0': format(0, '016b'),
+            'R1': format(1, '016b'),
+            'R2': format(2, '016b'),
+        }
+        self.SYMBOL_TABLE = {}
+        self.command = {
+            self.A_COMMAND : self.get_address_of_a_command,
+            self.C_COMMAND : self.get_address_of_c_command,
+            self.L_COMMAND : self.get_address_of_l_command,
+        }
 
 
-def get_advance(input_text):
-    command_type = get_command_type(input_text)
-    command = get_command(command_type, input_text)
-    return command
+    def is_defined_symbol_table(self, input_value):
+        return input_value in self.DEFINED_SYMBOL_TABLE
 
 
-def get_command_type(input_text):
-    if input_text[0] == '@':
-        return A_COMMAND
-    elif input_text[0] == '(':
-        return L_COMMAND
-    else:
-        return C_COMMAND
+    def is_symbol_table(self, input_value):
+        return input_value in self.SYMBOL_TABLE
 
 
-def get_command(command_type, input_text):
-    command = {
-        A_COMMAND : get_address_of_a_command,
-        C_COMMAND : get_address_of_c_command,
-        L_COMMAND : get_address_of_l_command,
-    }
-    return command[command_type](input_text)
+    def add_symbol_table(self, memonic, address):
+        self.SYMBOL_TABLE[memonic] = address
 
 
-def get_address_of_a_command(input_text):
-    input_value = input_text[1:]
-    try:
-        input_value = int(input_value)
-        value = format(input_value, '016b')
-        return value
-    except:
-        # @i のケース
-        return 'value'
+    def get_symbol_table(self):
+        return self.SYMBOL_TABLE
 
 
-def get_address_of_c_command(input_text):
-    command = '111'
-    if '=' in input_text:
-        dest_memonic, comp_memonic = input_text.split('=')
-        comp_binary = get_comp_binary(comp_memonic)
-        dest_binary = get_dest_binary(dest_memonic)
-        command += comp_binary + dest_binary + '000'
+    def get_advance(self, input_text):
+        command_type = self.get_command_type(input_text)
+        command = self.get_command(command_type, input_text)
         return command
-    elif ';' in input_text:
-        comp_memonic, jump_memonic = input_text.split(';')
-        comp_binary = get_comp_binary(comp_memonic)
-        jump_binary = get_jump_binary(jump_memonic)
-        command += comp_binary + '000' + jump_binary
-        return command
-    else:
-        raise ValueError('Input is invalid.')
 
 
-def get_address_of_l_command(input_text):
-    value = format(100, '016b')
-    return 'l'
+    def get_command_type(self, input_text):
+        if input_text[0] == '@':
+            return self.A_COMMAND
+        elif input_text[0] == '(' and input_text[-1] == ')':
+            return self.L_COMMAND
+        else:
+            return self.C_COMMAND
 
 
-def get_dest_command(input_text):
-    return 'get_dest_command'
+    def get_command(self, command_type, input_text):
+        return self.command[command_type](input_text)
 
 
-def get_comp_command(input_text):
-    return 'get_comp_command'
+    def get_address_of_a_command(self, input_text):
+        input_value = input_text[1:]
+        try:
+            input_value = int(input_value)
+            value = format(input_value, '016b')
+            return value
+        except:
+            # @i のケース
+            if self.is_defined_symbol_table(input_value):
+                return self.DEFINED_SYMBOL_TABLE[input_value]
+            elif self.is_symbol_table(input_value):
+                return self.SYMBOL_TABLE[input_value]
+            else:
+                raise ValueError('Input in get_address_of_a_command is invalid.')
 
 
-def get_jump_command(input_text):
-    return 'get_jump_command'
+    def get_address_of_c_command(self, input_text):
+        command = '111'
+        if '=' in input_text:
+            dest_memonic, comp_memonic = input_text.split('=')
+            comp_binary = get_comp_binary(comp_memonic)
+            dest_binary = get_dest_binary(dest_memonic)
+            command += comp_binary + dest_binary + '000'
+            return command
+        elif ';' in input_text:
+            comp_memonic, jump_memonic = input_text.split(';')
+            comp_binary = get_comp_binary(comp_memonic)
+            jump_binary = get_jump_binary(jump_memonic)
+            command += comp_binary + '000' + jump_binary
+            return command
+        else:
+            raise ValueError('Input is invalid.')
+
+
+    def get_address_of_l_command(self, input_text):
+        value = format(100, '016b')
+        return 'l'
+
+
+    def get_dest_command(self, input_text):
+        return 'get_dest_command'
+
+
+    def get_comp_command(self, input_text):
+        return 'get_comp_command'
+
+
+    def get_jump_command(self, input_text):
+        return 'get_jump_command'
