13b01ed81c2c39e84c7fb9594883be1d339b5d2e
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-01-27 05:26:45 +0900

    update parser

--- kiwata/07/vmtranslator/memonic_parser.py
+++ kiwata/07/vmtranslator/memonic_parser.py
@@ -20,6 +20,7 @@ class Parser:
             self.C_PUSH: self.get_assembly_of_push,
         }
         self.SEGMENT_CONSTANT = 9
+        self.ARITHMETIC_COUNTER = 0
 
 
     def get_advance(self, input_text):
@@ -27,6 +28,7 @@ class Parser:
         command_type = self.get_command_type(splited_input_text)
         command = self.get_command(command_type, splited_input_text)
         print(command)
+        print('-' * 20)
         return command
 
 
@@ -51,8 +53,26 @@ class Parser:
 
     def get_assembly_of_arithmetic(self, splited_input_text):
         arithmetic_command = splited_input_text[0]
+        # 算術演算
         if arithmetic_command == 'add':
             return self.get_assembly_of_add(arithmetic_command)
+        elif arithmetic_command == 'sub':
+            return self.get_assembly_of_sub(arithmetic_command)
+        elif arithmetic_command == 'neg':
+            return self.get_assembly_of_neg(arithmetic_command)
+        elif arithmetic_command == 'eq':
+            return self.get_assembly_of_eq(arithmetic_command)
+        elif arithmetic_command == 'lt':
+            return self.get_assembly_of_lt(arithmetic_command)
+        elif arithmetic_command == 'gt':
+            return self.get_assembly_of_gt(arithmetic_command)
+        # 論理演算
+        elif arithmetic_command == 'and':
+            return self.get_assembly_of_and(arithmetic_command)
+        elif arithmetic_command == 'or':
+            return self.get_assembly_of_or(arithmetic_command)
+        elif arithmetic_command == 'not':
+            return self.get_assembly_of_not(arithmetic_command)
         else:
             raise ValueError('Invalid input text.')
 
@@ -71,6 +91,201 @@ class Parser:
             '@SP',
             'M=M+1',
             ]
+        return assembly
+
+
+    def get_assembly_of_eq(self, arithmetic_command):
+        assembly = [
+            '@SP',
+            'M=M-1',
+            'A=M',
+            'D=M',
+            '@SP',
+            'M=M-1',
+            'A=M',
+            'D=D-M',
+            # 条件分岐
+            '@EQ{}'.format(self.ARITHMETIC_COUNTER), # when !=
+            'D;JEQ',
+            '@0',
+            'D=A',
+            '@SP',
+            'A=M',
+            'M=D',
+            '@SP',
+            'M=M+1',
+            '@NEXT{}'.format(self.ARITHMETIC_COUNTER), # 次のセクションへ飛ぶ
+            '0;JMP',
+            '(EQ{})'.format(self.ARITHMETIC_COUNTER), # when =
+            '@0',
+            'D=A',
+            'D=D-1',
+            '@SP',
+            'A=M',
+            'M=D',
+            '@SP',
+            'M=M+1',
+            '(NEXT{})'.format(self.ARITHMETIC_COUNTER),
+        ]
+        self.ARITHMETIC_COUNTER += 1
+        return assembly
+
+
+    def get_assembly_of_lt(self, arithmetic_command):
+        self.ARITHMETIC_COUNTER += 1
+        assembly = [
+            '@SP',
+            'M=M-1',
+            'A=M',
+            'D=M',
+            '@SP',
+            'M=M-1',
+            'A=M',
+            'D=D-M',
+            # 条件分岐
+            '@EQ{}'.format(self.ARITHMETIC_COUNTER), # when !=
+            'D;JGT',
+            '@0',
+            'D=A',
+            '@SP',
+            'A=M',
+            'M=D',
+            '@SP',
+            'M=M+1',
+            '@NEXT{}'.format(self.ARITHMETIC_COUNTER), # 次のセクションへ飛ぶ
+            '0;JMP',
+            '(EQ{})'.format(self.ARITHMETIC_COUNTER), # when true
+            '@0',
+            'D=A',
+            'D=D-1',
+            '@SP',
+            'A=M',
+            'M=D',
+            '@SP',
+            'M=M+1',
+            '(NEXT{})'.format(self.ARITHMETIC_COUNTER),
+        ]
+        return assembly
+
+
+    def get_assembly_of_gt(self, arithmetic_command):
+        self.ARITHMETIC_COUNTER += 1
+        assembly = [
+            '@SP',
+            'M=M-1',
+            'A=M',
+            'D=M',
+            '@SP',
+            'M=M-1',
+            'A=M',
+            'D=D-M',
+            # 条件分岐
+            '@EQ{}'.format(self.ARITHMETIC_COUNTER), # when !=
+            'D;JLT',
+            '@0',
+            'D=A',
+            '@SP',
+            'A=M',
+            'M=D',
+            '@SP',
+            'M=M+1',
+            '@NEXT{}'.format(self.ARITHMETIC_COUNTER), # 次のセクションへ飛ぶ
+            '0;JMP',
+            '(EQ{})'.format(self.ARITHMETIC_COUNTER), # when true
+            '@0',
+            'D=A',
+            'D=D-1',
+            '@SP',
+            'A=M',
+            'M=D',
+            '@SP',
+            'M=M+1',
+            '(NEXT{})'.format(self.ARITHMETIC_COUNTER),
+        ]
+        return assembly
+
+
+    def get_assembly_of_sub(self, arithmetic_command):
+        self.ARITHMETIC_COUNTER += 1
+        assembly = [
+            '@SP',
+            'M=M-1',
+            'A=M',
+            'D=M',
+            '@SP',
+            'M=M-1',
+            'A=M',
+            'D=M-D',
+            'M=D',
+            '@SP',
+            'M=M+1',
+        ]
+        return assembly
+
+
+    def get_assembly_of_neg(self, arithmetic_command):
+        self.ARITHMETIC_COUNTER += 1
+        assembly = [
+            '@SP',
+            'M=M-1',
+            'A=M',
+            'D=M',
+            'D=-D',
+            'M=D',
+            '@SP',
+            'M=M+1',
+        ]
+        return assembly
+
+
+    def get_assembly_of_and(self, arithmetic_command):
+        self.ARITHMETIC_COUNTER += 1
+        assembly = [
+            '@SP',
+            'M=M-1',
+            'A=M',
+            'D=M',
+            '@SP',
+            'M=M-1',
+            'A=M',
+            'D=D&M',
+            'M=D',
+            '@SP',
+            'M=M+1',
+        ]
+        return assembly
+
+
+    def get_assembly_of_or(self, arithmetic_command):
+        self.ARITHMETIC_COUNTER += 1
+        assembly = [
+            '@SP',
+            'M=M-1',
+            'A=M',
+            'D=M',
+            '@SP',
+            'M=M-1',
+            'A=M',
+            'D=D|M',
+            'M=D',
+            '@SP',
+            'M=M+1',
+        ]
+        return assembly
+
+
+    def get_assembly_of_not(self, arithmetic_command):
+        self.ARITHMETIC_COUNTER += 1
+        assembly = [
+            '@SP',
+            'M=M-1',
+            'A=M',
+            'D=M',
+            'D=!D',
+            'M=D',
+            '@SP',
+            'M=M+1',
+        ]
         return assembly
 
 
